2865a484ce7a0217fe539f1013a2a281d4c5b080
DominusSapiens <dominusmingo@gmail.com>
2026-01-01 21:06:26 -0300

    Actualizar busquedaVuelos.php

--- busquedaVuelos.php
+++ busquedaVuelos.php
@@ -2,6 +2,7 @@
 function cargardatos($id_vuelo, $Fecha, $origen, $destino){
 //rescata los datos ingresados por formulario.
 // detro de esta función se debe llamar a validarDatos().
+//retorna un objeto con los parámetros de búsqueda
 };
 function validarDatos($id_vuelo, $Fecha, $origen, $destino){
 // validar si los campos no están vacíos
@@ -9,13 +10,18 @@ function validarDatos($id_vuelo, $Fecha, $origen, $destino){
 // validar que el origen y destino sean diferentes
 // retornar true si todo es válido, false en caso contrario
 } ;
-function cargarVuelos(
+function cargarVuelos(){
     //Cargar los vuelos desde la base de datos.
     // Se busca el match con los datos ingresados.
     //imprime mnesaje  de error si no hay vuelos disponibles.
-){};
+			//retorna un array de vuelos
+};
 
-
+function compararConsulta(arrayVuelosBBDD, objetoConsulta){
+		//recibe desde cargar datos los parámetros de consulta
+		//y compara contra el array de cargar vuelos
+		//retorna un array de vuelos que coinciden con la bùsqueda
+		//o un array vacío si no existen coincidencias}
 
 
 
